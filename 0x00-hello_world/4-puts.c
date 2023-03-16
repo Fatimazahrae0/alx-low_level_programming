e89ae50ab90bdbb76d985a9665f42fff47dbaecc
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-16 11:46:53 -0700

    my first code with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/*
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char str[] = "programming is like building a multilingual puzzle";
+char str[] = "\"programming is like building a multilingual puzzle";
 puts(str);
 return (0);
 }
