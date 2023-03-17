d023b5866665e63aaf306ddf69d6001403f57aaa
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-17 12:02:01 -0700

    positive and negative with my beaytifull variables if, else if, else <3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,12 +4,18 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 if (n > 0)
 {
 /* if the condition is true then print the following */
@@ -26,4 +32,5 @@ else
 printf("%d is negative\n", n);
 }
 return (0);
+
 }
