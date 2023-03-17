ef69e7eeb2d016b13e7e74ec969e28340cda5633
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-17 16:25:27 -0700

    numbers with ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		/* My code is here  */
+	int x;
+
+	x = 48;
+	while (x <= 57)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
