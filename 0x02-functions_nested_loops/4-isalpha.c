ff200882c881e21f6c9bcbcbd02eda0e2c51f66a
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-21 14:36:05 -0700

    lowercase uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - Prints an integer to stdout
+ *
+ * @c: The integer to be printed
+ *
+ * Return: void
+ */
+int _isalpha(int c)
+{
+if (c <= 122 && c >= 97)
+{
+return (1);
+}
+else if (c <= 90 && c >= 65)
+{
+	return (1);
+}
+else
+{
+return (0);
+}
+putchar('\n');
+}
