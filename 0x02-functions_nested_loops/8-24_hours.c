1ac3a7734ffdaf3cec49297dc894446768fccb7e
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-21 16:42:59 -0700

    tack tock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints an integer to stdout
+ *
+ * @h @m: The integer to be printed
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+
+{
+int h, m;
+
+for (h = 0; h < 24; h++)
+{
+	for (m = 0; m < 60; m++)
+	{
+		_putchar(h / 10 + '0');
+		_putchar(h % 10 + '0');
+		_putchar(';');
+		_putchar(m / 10 + '0');
+		_putchar(m % 10 + '0');
+		_putchar('\n');
+}
+}
+}
+
