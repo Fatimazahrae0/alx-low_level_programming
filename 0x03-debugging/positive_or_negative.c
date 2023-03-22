3479d228880fcfc55a5111a10f7e52b9e6acddb6
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-22 10:00:55 -0700

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+{
+/* if the condition is true then print the following */
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+/* if the condition is true then print the following*/
+printf("%d is zero\n", i);
+}
+else
+{
+/*if all the conditions are false then print the following*/
+printf("%d is negative\n", i);
+};
+
+}
+
