ad2bea25db047201667f883498abb91233b8a4da
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-21 18:17:59 -0700

    add two integer and turn the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Prints an integer to stdout
+ *
+ * @a : The integer to be printed
+ * @b : the integer to be printed
+ *
+ * Return: void
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
