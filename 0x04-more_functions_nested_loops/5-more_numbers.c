ca1f177db4145fcd176a8374ca839d59b90a7a64
Fatimazahrae0 <126278756+Fatimazahrae0@users.noreply.github.com>
2023-03-23 20:15:01 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print a lines of alphabets
+ * @a: integer will be printed
+ * @b: integer will bw printed
+ * Return: always 0 success 
+*/
+void print_alphabet_x10(void)
+{
+    int a, b;
+
+    for (a = 0; a <= 9; a++)
+    {
+        for (b = 0; b <= 14; b++ )
+    _putchar(b);
+
+    }
+_putchar('\n');
+}
