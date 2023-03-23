b63f2e6e7cf77e2b3c9f289d1e64cd7c14ceabda
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-23 09:02:20 -0700

    most number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 0 to 9 including 4 and 2
+ *
+ * @x ; the nymbers will be printed
+ * Return: always success
+ */
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if (x != 50 && x != 54)
+		{
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
