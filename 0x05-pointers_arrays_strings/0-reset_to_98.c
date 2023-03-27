3de27fd7bcdb864cd844a8bccffeacc4d2fbe693
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-27 20:17:15 +0000

    ok

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,15 @@
 #include "main.h"
-
 /**
- *reset_to_98 : update the value of int to 98
- *@n: the integer will take the value
+ * reset_to_98 - Prints an integer to stdout
+ *
+ * @n: The integer to be printed
+ *
+ * Return: void
  */
+
 void reset_to_98(int *n)
 {
-*n = 98;;
+*n = 98;
 }
 
 
