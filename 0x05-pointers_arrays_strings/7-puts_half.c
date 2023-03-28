236b6294e927fbd3307ad925f863a04b1a98eabb
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-28 00:21:21 +0000

    half strinf <3

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _isalpha - Prints an integer to stdout
+ *
+ * @c: The integer to be printed
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{ 
+	int i;
+	int n;
+
+	i = 0;
+	while (str[i])
+		i++;
+	i++;
+	if (i % 2 == 1)
+		n = (i - 1) / 2;
+	else
+		n = i / 2;
+	while (str[n])
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
