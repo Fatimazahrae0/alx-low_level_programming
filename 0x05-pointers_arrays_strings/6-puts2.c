e09d08c89936d3c378bcd206f519634bdb6467c6
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-28 00:57:19 +0000

    print character<3

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - Prints an integer to stdout
+ *
+ * @str: The integer to be printed
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i;
+i = 0;
+
+while (str[i])
+{
+if (i % 2 == 0)
+
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+
+}
+
+
