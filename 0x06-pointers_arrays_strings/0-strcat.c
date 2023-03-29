56676124737c65d9880939993e88b6e0aae5de02
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-03-29 22:15:44 +0000

    strcat; concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two integers
+ * @src:parameter 1st string
+ * @dest:parameter 2nd string
+ * Return: return pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+while (*dest != '\0')
+{
+dest++;
+}
+for (; *src != '\0'; src++)
+{
+*dest = *src;
+dest++;
+}
+return (0);
+}
