1fa6f5bd3cfe83b61e05622dccd56f5f8669f34c
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-04-03 21:43:04 +0000

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,16 @@
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
+	int i;
 
-while (s[i])
-{
-if (s[i] == c)
-{
-return (s + i);
-}
-i++;
-}
-return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (NULL);
 }
