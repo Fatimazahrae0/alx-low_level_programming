5cc03e103cd1fbc2bc1dc5078b1cbbaa5d5d25f0
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-04-04 05:19:30 +0000

    searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_strpbrk - search a string for any of set of bytes
+* @s:the string
+* @accept:string
+* Return: ti s or null
+*/
+ char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+return (&s[i]);
+}
+}
+}
+return (NULL);
+
+}
+
