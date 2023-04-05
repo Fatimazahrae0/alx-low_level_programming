8982dc63de53916266ce9e06407bfd8330e0c5c4
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-04-05 20:37:42 +0000

    reversing a string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _print_rev_recursion - reversing a string
+*@s:string will be reversed
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+_print_rev_recursion(s + 1);
+_putchar(*s);
+
+}
