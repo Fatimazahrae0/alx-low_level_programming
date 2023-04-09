d897e988151464767783d4cad0131328215946a2
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-04-09 19:42:49 +0000

    a program prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,11 @@
 /*program that prints its name*/
 #include <stdio.h>
 
-int main(int argc,char *argv[]) {
+int main(int argc,char *argv[])
+{
 (void)argc;
 
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
 
