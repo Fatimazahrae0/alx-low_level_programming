58a562ac7f573d71ec236efc0a9d1dd9c8809081
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-04-09 20:51:08 +0000

    program that muyiple the numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that multiplies two numbers
+ * @argc:size of the arguments
+ * @argv:value of the argument
+ * Return:return void
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+
+	if (argc != 3)
+{
+	printf("Error\n");
+	return (1);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+
+result = num1 *num2;
+
+	printf("%d\n", result);
+return (0);
+}
