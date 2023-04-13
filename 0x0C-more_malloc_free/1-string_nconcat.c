b2aa71bf8b79f8fb4d7871e8dd62389d1d383116
fatimazahrae <ouadrassifatimazahrae7@gmail.com>
2023-04-13 10:53:27 +0100

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,22 +19,19 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-
 while (s1[len1] != '\0')
  {
 len1++;
 }
 while (s2[len2] != '\0')
 {
-len2++;
-    
+len2++;    
 }
 if (n >= len2)
 {
 n = len2;
 }
-concas = malloc((len1 + n + 1) * sizeof(char));
-
+concas = malloc((len1 + n + 1) * sizeof(char))
 if (concas == NULL)
 {
 return (NULL);
