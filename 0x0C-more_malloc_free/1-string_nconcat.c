c573644e374c6837254fbdbf9cf404ea2cc6c8b9
fatimazahrae <ouadrassifatimazahrae7@gmail.com>
2023-04-13 10:32:50 +0100

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,31 +8,30 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-   unsigned int len1 = 0, len2 = 0, i, j;
-    char *concas;
+unsigned int len1 = 0, len2 = 0, i, j;
+char *concas;
 if (s1 == NULL)
 {
-    s1 = "";
+s1 = "";
 }
 if (s2 == NULL)
 {
-    s2 = "";
+s2 = "";
 }
 
-    while (s1[len1] != '\0')
-    {
-    len1++;
-    }
-    while (s2[len2] != '\0')
-    {
-        len2++;
+while (s1[len1] != '\0')
+ {
+len1++;
+}
+while (s2[len2] != '\0')
+{
+len2++;
     
-    }
-    if (n >= len2)
-    {
-        n = len2;
-    }
-
+}
+if (n >= len2)
+{
+n = len2;
+}
 concas = malloc((len1 + n + 1) * sizeof(char));
 
 if (concas == NULL)
@@ -41,11 +40,12 @@ return (NULL);
 }
 for (i = 0; i < len1; i++)
 {
-s1[i] = concas[i];
+    concas[i] = s1[i];
 }
 for (j = 0; j < n; i++, j++)
 {
     concas[i] = s2[j];
 }
+concas[i] = '\0';
 return (concas);
 }
