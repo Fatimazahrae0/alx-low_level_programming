c2253c4bc4ddc02b50b491c5e9da7d872067f465
fatimazahrae <ouadrassifatimazahrae7@gmail.com>
2023-04-13 10:22:29 +0100

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,14 +6,13 @@
 */
 void *malloc_checked(unsigned int b)
 {
-    int *ptr;
 
-    ptr = malloc(b);
+int *ptr = malloc(b);
     
     if (ptr == NULL)
-    {
+    
         exit(98);
-    }
+    
 return (ptr);
 }
 
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * string_nconcat - concatenates two strings
+ * @s1:first string
+ * @s2:second string
+ * @n:size of string
+ * Return:return NULL if the function fails
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+   unsigned int len1 = 0, len2 = 0, i, j;
+    char *concas;
+if (s1 == NULL)
+{
+    s1 = "";
+}
+if (s2 == NULL)
+{
+    s2 = "";
+}
+
+    while (s1[len1] != '\0')
+    {
+    len1++;
+    }
+    while (s2[len2] != '\0')
+    {
+        len2++;
+    
+    }
+    if (n >= len2)
+    {
+        n = len2;
+    }
+
+concas = malloc((len1 + n + 1) * sizeof(char));
+
+if (concas == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len1; i++)
+{
+s1[i] = concas[i];
+}
+for (j = 0; j < n; i++, j++)
+{
+    concas[i] = s2[j];
+}
+return (concas);
+}
