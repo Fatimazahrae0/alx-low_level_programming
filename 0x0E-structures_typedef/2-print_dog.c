27caaeb07b18c6e4bd9a09e53d8ad50b6f701f2e
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-04-17 21:07:46 +0000

    fati

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,23 +7,26 @@
 */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-    {
-        return ;
-    }
-    if (d->name == NULL)
+if (d == NULL)
 {
-        printf("Name: (nil)");
+return ;
 }
-    else 
+if (d->name == NULL)
 {
-    printf("Name: %s\n", d->name);
+printf("Name: (nil)");
 }
-    printf("Age: %f\n", d->age);
+else
+{
+printf("Name: %s\n", d->name);
+}
+printf("Age: %.6f\n", d->age);
 
-    if (d->owner == NULL)
- {
-    printf("Owner: %s\n", d->owner);
- }
- 
-    }
+if (d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
