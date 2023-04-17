fd6bd3f1fab4bdf3b6dfd26ee320ed1f8555676a
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-04-17 23:49:20 +0000

    frees the dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,10 +6,10 @@
 */
 void free_dog(dog_t *d)
 {
-    if (!d)
-        return;
-    free(d->name);
-    free(d->owner);
-    free(d);
+if (!d)
+return;
+free(d->name);
+free(d->owner);
+free(d);
 
 }
