c8f42c9d42aa154f6e0197c1a7d1702eff15f735
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-04-20 00:00:10 +0000

    <3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -40,10 +40,8 @@ int op_div(int a, int b)
 {
 if (b == 0)
 {
-printf("Error\n");
-exit(100);
-}
 return (a / b);
+}
 }
 /**
  * op_mod - modulo division of two numbers
@@ -55,8 +53,6 @@ int op_mod(int a, int b)
 {
 if (b == 0)
 {
-printf("Error\n");
-exit(100);
-}
 return (a % b);
+}
 }
