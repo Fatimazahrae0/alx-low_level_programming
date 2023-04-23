9b5c87afa5075ec1e45f8692d5414066f9a88734
ouadrassifatimazahrae7@gmail.com <ouadrassifatimazahrae7@gmail.com>
2023-04-23 18:17:03 +0100

    print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 /**
 * print_all - Prints all of the arguments when specified
 * @format: specifies the necessary operations
@@ -7,42 +8,41 @@
 */
 void print_all(const char * const format, ...)
 {
-	va_list arglist;
-	int n = 0, i = 0;
-	char *separator = ", ";
-	char *str;
+va_list arglist;
+int n = 0, i = 0;
+char *separator = ", ";
+char *str;
 
-	va_start(arglist, format);
+va_start(arglist, format);
 
-	while (format && format[i])
-		i++;
-
-	while (format && format[n])
-	{
-		if (n  == (i - 1))
-		{
-			separator = "";
-		}
-		switch (format[n])
-		{
-		case 'c':
-			printf("%c%s", va_arg(arglist, int), separator);
-			break;
-		case 'i':
-			printf("%d%s", va_arg(arglist, int), separator);
-			break;
-		case 'f':
-			printf("%f%s", va_arg(arglist, double), separator);
-			break;
-		case 's':
-			str = va_arg(arglist, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", str, separator);
-			break;
-		}
-		n++;
-	}
-	printf("\n");
-	va_end(arglist);
+while (format && format[i])
+i++;
+while (format && format[n])
+{
+if (n  == (i - 1))
+{
+separator = "";
+}
+switch (format[n])
+{
+case 'c':
+printf("%c%s", va_arg(arglist, int), separator);
+break;
+case 'i':
+printf("%d%s", va_arg(arglist, int), separator);
+break;
+case 'f':
+printf("%f%s", va_arg(arglist, double), separator);
+break;
+case 's':
+str = va_arg(arglist, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s%s", str, separator);
+break;
+}
+n++;
+}
+printf("\n");
+va_end(arglist);
 }
