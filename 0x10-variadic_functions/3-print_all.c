53ed16486d4ca91c8221c422be1b0561d49c7894
ouadrassifatimazahrae7@gmail.com <ouadrassifatimazahrae7@gmail.com>
2023-04-23 18:11:09 +0100

    print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,46 +6,42 @@
 */
 void print_all(const char * const format, ...)
 {
-int i;
-int flag;
-char *str;
+	va_list arglist;
+	int n = 0, i = 0;
+	char *separator = ", ";
+	char *str;
 
-va_list a_list;
+	va_start(arglist, format);
 
-va_start(a_list, format);
-i = 0;
+	while (format && format[i])
+		i++;
 
-while (format != NULL && format[i] != '\0')
-{
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(a_list, int));
-flag = 0;
-break;
-case 'i':
-printf("%i", va_arg(a_list, int));
-flag = 0;
-break;
-case 'f':
-printf("%f", va_arg(a_list, double));
-flag = 0;
-break;
-case 's':
-str = va_arg(a_list, char*);
-if (str == NULL)
-str = "(nil)";
-printf("%s", str);
-flag = 0;
-break;
-default:
-flag = 1;
-break;
-}
-if (format[i + 1] != '\0' && flag == 0)
-printf(", ");
-i++;
-}
-printf("\n");
-va_end(a_list);
+	while (format && format[n])
+	{
+		if (n  == (i - 1))
+		{
+			separator = "";
+		}
+		switch (format[n])
+		{
+		case 'c':
+			printf("%c%s", va_arg(arglist, int), separator);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(arglist, int), separator);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(arglist, double), separator);
+			break;
+		case 's':
+			str = va_arg(arglist, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", str, separator);
+			break;
+		}
+		n++;
+	}
+	printf("\n");
+	va_end(arglist);
 }
