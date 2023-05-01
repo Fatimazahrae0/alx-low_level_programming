85df2433fe760edc3374fa97584a8d9df19c4fac
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-05-01 17:15:57 +0100

    Task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,17 @@
-#include "lists.h"
-/**
- * free_listint - frees a linked list
- * @head:head of linked list
- * Return:return (head)
-*/
-void free_listint(listint_t *head)
-{
-listint_t *tmp;
+    #include "lists.h"
+    /**
+     * free_listint - frees a linked list
+     * @head:head of linked list
+     * Return:return (head)
+    */
+    void free_listint(listint_t *head)
+    {
+    listint_t *tmp;
 
-while (head != NULL)
-{
+    while (head != NULL)
+    {
     tmp = head;
     head = head->next;
     free(tmp);
-}
-}
+    }
+    }
