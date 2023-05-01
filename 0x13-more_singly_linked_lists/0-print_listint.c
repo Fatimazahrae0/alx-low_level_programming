11cfce87e7cd23bf285505b043f60d47b9d096f2
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-05-01 15:33:32 +0100

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 /**
- * print_listint - 
- * 
+ * print_listint - print nodes
+ * @h:element of linked lists
+ * Return:return to the node
 */
 size_t print_listint(const listint_t *h)
 {
 size_t count = 0;
 
-while(h != NULL)
+while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
 count++;
 }
-return(count);
+return (count);
 }
