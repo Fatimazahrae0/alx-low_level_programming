2d6b40770803efbe49189a5c0518786c26b7930f
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-05-08 20:35:40 +0100

    Task 1 <3

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - Creates a file with the specified text content
+ * @filename: Name of the file to create
+ * @text_content: Content to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int f, i;
+
+if (filename == NULL)
+return (-1);
+f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if(f == -1)
+return (-1);
+if (text_content != NULL)
+{
+for (i = 0; text_content[i]; i++)
+continue;
+if (write(f, text_content, i) != i)
+{
+close(f);
+return (-1);
+}
+}
+
+close(f);
+return (1);
+}
