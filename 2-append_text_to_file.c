eab8c12e6c89935f0c5728f90f0f6e2be4f54467
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-05-08 22:00:21 +0100

    Task 2 <3

--- 2-append_text_to_file.c
+++ 2-append_text_to_file.c
@@ -7,32 +7,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int f, i, len;
+int fd, i, len;
 
 if (filename == NULL)
 return (-1);
 
-
 fd = open(filename, O_WRONLY | O_APPEND);
-if (f == -1)
+if (fd == -1)
 return (-1);
 
 if (text_content != NULL)
 {
-
 for (len = 0; text_content[len]; len++)
 continue;
-
-if (write(f, text_content, len) != len)
+if (write(fd, text_content, len) != len)
 {
-
-close(f);
+close(fd);
 return (-1);
 }
-
-
 }
 
-close(f);
+close(fd);
 return (1);
 }
