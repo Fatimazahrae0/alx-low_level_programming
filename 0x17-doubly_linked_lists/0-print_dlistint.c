daaa9aa5d2e2823f10f3439d495829c6f22c2659
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-06-15 18:14:01 +0100

    Fixed (betty linker)

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,16 +2,17 @@
 /**
  * print_dlistint - print element in the linked list
  * @h: head of the linked  list
- * return the number of nodes
+ * Return: the number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
 size_t count_n = 0;
+
 while (h != NULL)
 {
-    printf("%d\n", h->n);
-    h = h->next;
-    count_n++;
+printf("%d\n", h->n);
+h = h->next;
+count_n++;
 }
 return (count_n);
 }
