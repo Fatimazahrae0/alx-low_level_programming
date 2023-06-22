731346e807ceaaa845317c249e20db065c9ad506
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-06-22 14:47:57 +0100

    done

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,43 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Deletes the node at a given index.
- * @head: Double pointer to the head node of the list.
- * @index: Index of the node to be deleted.
- * Return: 1 if deletion succeeded, -1 if it failed.
- */
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list.
+ * @head: pointer to the list.
+ * @index: position of the node to delete.
+ * Return: 1 if it succeeded, -1 if it failed.
+ **/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *current, *temp;
-unsigned int i;
+	dlistint_t *aux_node = *head;
+	dlistint_t *node_to_delete = *head;
+	unsigned int idx;
+	unsigned int cont = 0;
 
-if (head == NULL || *head == NULL)
-return (-1);
+	if (!(*head))
+		return (-1);
+	if (index == 0)
+	{
+		*head = node_to_delete->next;
+		free(node_to_delete);
+		if (*head)
+			(*head)->prev = NULL;
+		return (1);
+	}
+	idx = index - 1;
+	while (aux_node && cont != idx)
+	{
+		cont++;
+		aux_node = aux_node->next;
+	}
+	if (cont == idx && aux_node)
+	{
+		node_to_delete = aux_node->next;
+		if (node_to_delete->next)
+		node_to_delete->next->prev = aux_node;
+		aux_node->next = node_to_delete->next;
+		free(node_to_delete);
+		return (1);
+	}
 
-if (index == 0)
-{
-temp = *head;
-*head = (*head)->next;
-if (*head != NULL)
-(*head)->prev = NULL;
-free(temp);
-return (1);
-}
-
-current = *head;
-for (i = 0; i < index - 1 && current != NULL; i++)
-current = current->next;
-
-
-if (current == NULL || current->next == NULL)
-return (-1);
-
-temp = current->next;
-current->next = temp->next;
-if (temp->next != NULL)
-temp->next->prev = current;
-free(temp);
-return (1);
+	return (-1);
 }
