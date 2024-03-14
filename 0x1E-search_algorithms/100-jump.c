30e4c0630916422c8b6d1cb06ecb7e0e8142f2a1
Fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2024-03-14 15:32:11 +0000

    done

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,29 +9,34 @@
  * @value: is the value to search for
  * Return: the first index where value is located
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, step, jump;
+    size_t i, step, jump;
 
-	if (array == NULL || size == 0)
-		return (-1);
+    if (array == NULL || size == 0)
+        return (-1);
 
-	step = sqrt(size);
+    step = sqrt(size);
 
-	for (i = jump = 0; jump < size && array[jump] < value;)
-	{
-		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
-		i = jump;
-		jump += step;
-	}
+    // Loop to find the range where the value may be
+    for (i = jump = 0; jump < size && array[jump] < value;)
+    {
+        printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+        i = jump;
+        jump += step;
+    }
 
-	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+    // Print the range where the value may lie
+    printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
 
-	jump = jump > size - 1 ? jump : size - 1;
-	for (; i < jump && array[i] < value; i++)
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+    // Adjusting the boundary if jump exceeds the size of the array
+    jump = jump > size - 1 ? jump : size - 1;
 
-	return (array[i] == value ? (int)i : -1);
+    // Linear search within the identified range
+    for (; i < jump && array[i] < value; i++)
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+    printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+    // Check if the value is found and return its index
+    return (array[i] == value ? (int)i : -1);
 }
